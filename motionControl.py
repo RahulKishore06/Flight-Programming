6992b5e8d907e7685094ca76e0509a1688cc0fdb
Ethan Harjabrata <ethanjhar@gmail.com>
2025-11-07 18:12:02 -0600

    temp

--- motionControl.py
+++ motionControl.py
@@ -9,23 +9,17 @@ def main():
 
     #This is for controlling rate of roll
     kP_roll= 0
-    kI_roll=0
+    kD_roll=0
 
     #This is for controlling yaw/pitch
     kP_yaw= 0
-    kI_yaw=0
+    kD_yaw=0
 
     #This is the minimum altitude (in meters) before the rocket is allowed to turn on AFS
     min_alt=10 #since this is for a hypothetical L2 launch, the min_alt is probably just a couple meters above
     # the launch rail
 
     #initialize some values
-
-    #This is the integral term for the yaw controller
-    yaw_integral=0
-
-    #This is the integral term for the pitch controller
-    pitch_integral=0
 
     #this is the maximum error tolerated in pitch and yaw
     max_yaw_error=0.01 
