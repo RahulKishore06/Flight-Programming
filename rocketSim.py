47dc88e34ae8bf928aca8a6ba1e0c075d734600e
Ethan Harjabrata <ethanjhar@gmail.com>
2025-11-12 09:28:29 -0600

    re-encapsulated precise rocket and motor details inside functions
    Makes it a lot easier to just swap out the motor as needed.
    Will break down the makeDefaultRocket function later

--- rocketSim.py
+++ rocketSim.py
@@ -6,140 +6,112 @@ from scipy.signal import savgol_filter
 #dfhtmfuy
 from rocketpy import Rocket, Flight, Function, TrapezoidalFins, SolidMotor, Motor, Environment
 
-# def makeDefaultRocket(motor:Motor , fins: TrapezoidalFins, fin_positions: list = [], sensors: list = []):
-#     '''Set up the rocket to launch in the sim'''
-#     #REMIND ME TO FILL IN ACTUAl VALUES
-#     # default values
-#     rocket = Rocket(
-#         radius= 0.0381,
-#         mass= 1.04,
-#         inertia= 0,
-#         power_off_drag= (),
-#         power_on_drag=0,
-#         center_of_mass_without_motor=0,
-#         coordinate_system_orientation="tail_to_nose"
-#     )
-#     # add nose
-#     rocket.add_nose(length=0,kind="",position=0,bluffness=0.0,power=0.0)
+def makeDefaultRocket(motor:Motor , numFins: int, sensors: list = []):
+    '''
+    Set up the rocket to launch in the sim
+    motor: An instance of a motor class (SolidMotor etc)
+    numFins: The number of fins to be used on the rocket (3 or 4)
+    sensors: A list of sensors on the rocket'''
+    #REMIND ME TO FILL IN ACTUAl VALUES
+    # default values
+    rocket = Rocket(
+        radius=0.0655,
+        mass=24.05,
+        inertia=(15.07, 15.07, 0.067),
+        power_off_drag=0.65,
+        power_on_drag=0.65,
+        center_of_mass_without_motor=0,
+        coordinate_system_orientation="tail_to_nose",
+    )
 
+    factor = 0.38 / rocket.power_off_drag(0.6)  # From CFD analysis
+    rocket.power_on_drag *= factor
+    rocket.power_off_drag *= factor
 
-#     # add motor
-#     rocket.add_motor(motor, 0)
+    # add nose
+    rocket.add_nose(
+        length=0.565,
+        kind="vonKarman",
+        position=1.477,
+    )
+
+    # add tail
+    rocket.add_tail(
+        top_radius=0.0655, bottom_radius=0.0535, length=0.068, position=-1.226
+    )
+
+    # add motor
+    rocket.add_motor(motor, 0)
     
-#     rocket.add_surfaces(fins,fin_positions)
+    # add parachute
+    rocket.add_parachute(
+        "Drogue",
+        cd_s=0.885,
+        trigger="apogee",
+        sampling_rate=105,
+        noise=(0, 8.3, 0.5),
+        lag=0.5,
+    )
+
+    # add fins
+    rocket.add_trapezoidal_fins(
+        n=numFins,
+        root_chord=0.20,
+        tip_chord=0.12,
+        span=0.130,
+        position=-0.928,
+        cant_angle=0,
+        airfoil=(Function([[0, 0.0002], [2, 0.3320], [4, 0.6335], [6, 0.6877]]), "degrees"),
+    )
     
-#     # add sensors
-#     for sensor in sensors:
-#         rocket.add_sensor(sensor)
-
-# r1 = makeDefaultRocket()
-
-# def makeMotor(option):
-#     '''
-#     Set up the rocket motor to be simulated
-#     Available options are 
-#     1. Cert
-#     2. Spaceshot (TO BE ADDED!)
-#     '''
-#     if option=="Cert":
-#         motor = SolidMotor(
-#             thrust_source="FILENAME HERE!",
-#             dry_mass=0,
-#             dry_inertia=(0),
-#             nozzle_radius=0,
-#             grain_number=0,
-#             grain_density=0,
-#             grain_outer_radius=0,
-#             grain_initial_inner_radius=0,
-#             grain_initial_height=0,
-#             grain_separation=0,
-#             grains_center_of_mass_position=0,
-#             center_of_dry_mass_position=0,
-#             nozzle_position=0,
-#             burn_time=0,
-#             throat_radius=0,
-#             coordinate_system_orientation="nozzle_to_combustion_chamber"
-#         )
-#         return motor
-#     else:
-#         raise ValueError("Motor option not recognised")
+    if (len(sensors)!=0):
+        # add sensors
+        for sensor in sensors:
+            rocket.add_sensor(sensor)
+    return rocket
 
 
 
+def makeMotor(option):
+    '''
+    Set up the rocket motor to be simulated
+    Available options are 
+    1. Test
+    2. Cert (TO BE ADDED!)
+    3. Spaceshot (TO BE ADDED!)
+    '''
+    if option=="Test":
+        #update values as required
+        motor = SolidMotor(
+            thrust_source = "AeroTech_H242T.csv",
+            reshape_thrust_curve=(5.8, 8800),
+            grain_number=5,
+            grain_separation=0.006,
+            grain_outer_radius=0.0465,
+            grain_initial_inner_radius=0.016,
+            grain_initial_height=0.156,
+            grain_density=1748.9,
+            nozzle_radius=0.0335,
+            throat_radius=0.0114,
+            interpolation_method="linear",
+            dry_mass=0.00000000001,
+            grains_center_of_mass_position=-0.683,
+            center_of_dry_mass_position=-0.683,
+            dry_inertia=(0.0000000000001, 0.0000000000001, 0.0000000000001),
+            nozzle_position=-1.294,
+        )
+        return motor
+    if option=="Cert":
+        raise ValueError("Motor option not recognised")
+    else:
+        raise ValueError("Motor option not recognised")
 
+motor = makeMotor("Test")
 
-motor_H242T = SolidMotor(
-    thrust_source = "AeroTech_H242T.csv",
-    reshape_thrust_curve=(5.8, 8800),
-    grain_number=5,
-    grain_separation=0.006,
-    grain_outer_radius=0.0465,
-    grain_initial_inner_radius=0.016,
-    grain_initial_height=0.156,
-    grain_density=1748.9,
-    nozzle_radius=0.0335,
-    throat_radius=0.0114,
-    interpolation_method="linear",
-    dry_mass=0.00000000001,
-    grains_center_of_mass_position=-0.683,
-    center_of_dry_mass_position=-0.683,
-    dry_inertia=(0.0000000000001, 0.0000000000001, 0.0000000000001),
-    nozzle_position=-1.294,
-)
-# motor_H242T.draw()
+r1= makeDefaultRocket(motor, 4, [])
 
-drag_curve = .65
-
-
-r1 = Rocket(
-    radius=0.0655,
-    mass=24.05,
-    inertia=(15.07, 15.07, 0.067),
-    power_off_drag=drag_curve,
-    power_on_drag=drag_curve,
-    center_of_mass_without_motor=0,
-    coordinate_system_orientation="tail_to_nose",
-)
-r1.add_motor(motor_H242T, 0)
-
-r1.add_nose(
-    length=0.565,
-    kind="vonKarman",
-    position=1.477,
-).draw()
-
-    
-r1.add_trapezoidal_fins(
-    n=4,
-    root_chord=0.20,
-    tip_chord=0.12,
-    span=0.130,
-    position=-0.928,
-    cant_angle=0,
-    airfoil=(Function([[0, 0.0002], [2, 0.3320], [4, 0.6335], [6, 0.6877]]), "degrees"),
-).draw()
-
-
-tail = r1.add_tail(
-    top_radius=0.0655, bottom_radius=0.0535, length=0.068, position=-1.226
-)
-
-drogue = r1.add_parachute(
-    "Drogue",
-    cd_s=0.885,
-    trigger="apogee",
-    sampling_rate=105,
-    noise=(0, 8.3, 0.5),
-    lag=0.5,
-)
-
-factor = 0.38 / r1.power_off_drag(0.6)  # From CFD analysis
-
-r1.power_on_drag *= factor
-r1.power_off_drag *= factor
-
-r1.info()
-r1.draw()
+# r1.info()
+# r1.draw()
 
 # --------------------------------------------------------------
 # --------------------------------------------------------------
@@ -153,13 +125,12 @@ env = Environment(
     elevation=407,
 )
 
-
 env.max_expected_height = 1000
     
 env.set_date(date=(2025, 10, 23, 17), timezone="America/Denver")
 
     
-est_flight = Flight(
+test_flight = Flight(
     rocket=r1,
     environment=env,
     inclination=85,
@@ -170,4 +141,4 @@ est_flight = Flight(
     rail_length=5.2,
 )    
 
-print(est_flight.plots.trajectory_3d())
+print(test_flight.plots.trajectory_3d())
